d75f3ae37689bda201d760f882a815f666268661
sucessosandraalves-stack <sucessosandraalves@gmail.com>
2025-10-28 18:54:59 -0400

    Create Termos.js

---
+++ src/components/Termos.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Termos() {
+  return (
+    <div>
+      <h2>Termos e Condições</h2>
+      <p>
+        Ao utilizar os serviços da ContabilizaDigital, você concorda com nossos termos de uso, incluindo o uso de inteligência artificial auxiliar para recomendações. 
+        A responsabilidade final sobre decisões contábeis é do cliente e do contador responsável.
+      </p>
+    </div>
+  );
+}
+
+export default Termos;
