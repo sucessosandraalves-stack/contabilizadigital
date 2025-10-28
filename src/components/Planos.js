78701660c00a96019d54173332449d754735c565
sucessosandraalves-stack <sucessosandraalves@gmail.com>
2025-10-28 18:53:58 -0400

    Create Planos.js

---
+++ src/components/Planos.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Planos() {
+  return (
+    <div>
+      <h2>Planos Mensais</h2>
+      <ul>
+        <li><strong>Básico:</strong> R$199/mês – Suporte contábil online</li>
+        <li><strong>Intermediário:</strong> R$349/mês – Suporte contábil + relatórios financeiros</li>
+        <li><strong>Avançado:</strong> R$699/mês – Tudo incluído + consultoria personalizada</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Planos;
