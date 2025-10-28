0010dff1f18f52071ea23cf9b35296d48b632aeb
sucessosandraalves-stack <sucessosandraalves@gmail.com>
2025-10-28 18:54:35 -0400

    Create Contato.js

---
+++ src/components/Contato.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Contato() {
+  return (
+    <div>
+      <h2>Contato</h2>
+      <p>Telefone: +55 67 98223-6164</p>
+      <p>Email: sandra.diretora@hotmail.com</p>
+    </div>
+  );
+}
+
+export default Contato;
